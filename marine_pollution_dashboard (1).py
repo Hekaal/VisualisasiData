1b2ee998208672fe990b4988427e71cc82ceb4f8
Hekaal <haikalmsyaed@gmail.com>
2025-06-03 11:50:40 +0700

    Update marine_pollution_dashboard (1).py

--- marine_pollution_dashboard (1).py
+++ marine_pollution_dashboard (1).py
@@ -81,7 +81,6 @@ def load_data():
 df = load_data()
 
 st.title("🌍 Marine Pollution Dashboard")
-st.caption=("🌊 Lautan adalah paru-paru bumi kita.")
 st.markdown("Dashboard ini menampilkan visualisasi interaktif mengenai insiden polusi laut.")
 
 st.sidebar.header("Filter Data")
